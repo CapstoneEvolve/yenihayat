a5a632f7085bf23f9933ca7d6800ec168c87aa3b
Shpetim Halidi <shpetimhalidi390@gmail.com>
2024-12-05 14:46:48 +0100

    Making a better design for the hero section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
-const slides = document.querySelectorAll('.slide');
-const prevBtn = document.querySelector('.btn.prev');
-const nextBtn = document.querySelector('.btn.next');
-
-let currentIndex = 0;
-
-function updateSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-}
-
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    updateSlide(currentIndex);
-});
-
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slides.length;
-    updateSlide(currentIndex);
-});
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
