192b0140f1da2da589edfb19a16758e03b447fe4
Shpetim Halidi <shpetimhalidi390@gmail.com>
2024-12-12 20:52:51 +0100

    Making a better design for the Hero Section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-const swiper = new Swiper('.swiper', {
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
